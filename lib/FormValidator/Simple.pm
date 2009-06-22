6b8f1a21a6d6ed85556c2b07734a2a43aeee0def
lyokato <lyo.kato@gmail.com>
2009-06-22 14:39:14 +0900

    Checking in changes prior to tagging of version 0.25.  Changelog diff is:
    diff --git a/Changes b/Changes
    index dbbaadd..04ad050 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,6 @@
     Revision history for Perl extension FormValidator::Simple.
    
    -0.24  Mon Jun 22 14:30:00 2009
    +0.25  Mon Jun 22 14:30:00 2009
         - bugfix: on single apache process, 'each' command in Profile.pm breaks
           hash reference.
           Thanks to asannou.

--- lib/FormValidator/Simple.pm
+++ lib/FormValidator/Simple.pm
@@ -12,7 +12,7 @@ use FormValidator::Simple::Validator;
 use FormValidator::Simple::Constants;
 use FormValidator::Simple::Messages;
 
-our $VERSION = '0.24';
+our $VERSION = '0.25';
 
 __PACKAGE__->mk_classaccessors(qw/data prof results/);
 __PACKAGE__->mk_classaccessor( messages => FormValidator::Simple::Messages->new );
